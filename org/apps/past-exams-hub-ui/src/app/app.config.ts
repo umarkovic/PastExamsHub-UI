30c6b272f28ae60fa3eb7fd2be7d9e31f5cb071f
umarkovic <umarkovic864@live.com>
2024-02-04 18:55:37 +0100

    new stuff added

--- org/apps/past-exams-hub-ui/src/app/app.config.ts
+++ org/apps/past-exams-hub-ui/src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { AuthenticationService } from './shared/services/authentication.service';
+import { environment } from '../environments/environment';
+
+function configureAuth(authenticationService: AuthenticationService) {
+  return () => authenticationService.configureAuth(environment.authorityApiUrl);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideAnimations()],
+  providers: [
+    provideRouter(appRoutes),
+    provideAnimations(),
+    provideHttpClient(),
+    provideOAuthClient({
+      resourceServer: {
+        allowedUrls: [environment.coreApiUrl],
+        sendAccessToken: true,
+      }
+    }),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      multi: true,
+      deps: [AuthenticationService],
+    },
+    
+  ],
 };
